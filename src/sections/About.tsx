719de4f695b4461889f7c07baa30eefa0605d4ff
Henry Web Developer <inyamahhenry20@gmail.com>
2025-04-17 10:37:50 +0100

    Update About.tsx

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -92,7 +92,7 @@ const hobbies = [
 
 export const AboutSection = () => {
   const constraintRef = useRef(null);
-  return <div className="py-20 lg:py-28">
+  return <div className="py-20 lg:py-28" id="about">
     <div className="container">
     <SectionHeader 
     eyebrow="About Me"
